cdf83fd87b46890f34001d53893ebb2d6c4ceee7
Zuleidy Yaruro <49843884+zuleidyyaruro@users.noreply.github.com>
2023-03-01 09:40:50 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,13 +25,13 @@ export class AppComponent {
   async onClickSave() {
 
     await saveDocument();
+    debugger;
     let datos = localStorage.getItem("datos");
-
     let objeto =
     {
-      "id": 4,
+      "id": 11,
       "username": "zuleidy.yaruro@maggioli-latam.com",
-      "denominacion": "Prueba",
+      "denominacion": "Prueba 6",
       "plantilla": datos,
       "editable": true,
       "observaciones": null,
@@ -41,7 +41,6 @@ export class AppComponent {
       "borrado": false,
       "id_fk_usuario": 3
     }
-
     if (objeto.id > 0) {
       this._sTextControl.updateDocumentoPlantilla(objeto).subscribe(res => {
         console.log(res)
@@ -60,10 +59,19 @@ export class AppComponent {
   onClickLoad() {
     console.log("1")
     const objetoModificar = {
-      "id": 4,
+      "id": 11,
       "username": "zuleidy.yaruro@maggioli-latam.com",
       "denominacion": "Prueba",
-      "plantilla": "<p>sajdhsajdhkjasdasdaszxczxczx</p>",
+      "plantilla":
+        "{\rtf1\ansi\ansicpg1252\\uc1\deff0\adeff0\deflang0\deflangfe0\adeflang0{\fonttbl" +
+        "{\f0\fswiss\fcharset0\fprq2{\*\panose 020B0604020202020204}Arial;}" +
+        "{\f1\froman\fcharset2\fprq2{\*\panose 05050102010706020507}Symbol;}}" +
+        "{\colortbl;\red0\green0\blue0;}" +
+        "{\stylesheet{\s0\ltrpar\itap0\nowidctlpar\ql\li0\ri0\lin0\rin0\cbpat0\rtlch\af0\afs24\ltrch\f0\fs24\lang1033\langnp1033\langfe1033\langfenp1033 [Normal];}{\*\cs10\additive Default Paragraph Font;}}" +
+        "{\info" +
+        "{\*\txInfo{\txVer 31.0.822.500}}}" +
+        "\paperw12240\paperh15840\margl1440\margt1440\margr1440\margb1440\deftab1134\widowctrl\trackmoves0\trackformatting1\lytexcttp\formshade\sectd" +
+        "\headery567\footery567\pgwsxn12240\pghsxn15840\marglsxn1440\margtsxn1440\margrsxn1440\margbsxn1440\pgbrdropt32\pard\ltrpar\itap0\nowidctlpar\ql\li0\ri0\lin0\rin0\plain\rtlch\af0\afs20\alang1033\ltrch\f0\fs20\lang1033\langnp1033\langfe1033\langfenp1033 creaci\loch\af0\dbch\af0\hich\f0\'f3\hich\af0\dbch\af0\loch\f0 n prueba}",
       "editable": true,
       "observaciones": null,
       "grupo_documento_plantilla": 2,
--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,7 +1,7 @@
 async function saveDocument() {
 
-  // TXTextControl.saveDocument(TXTextControl.StreamType.RichTextFormat, --> formato rtf
-  TXTextControl.saveDocument(TXTextControl.StreamType.HTMLFormat,
+  TXTextControl.saveDocument(TXTextControl.StreamType.RichTextFormat,
+    // TXTextControl.saveDocument(TXTextControl.StreamType.HTMLFormat,
     function (e) {
       localStorage.setItem("datos", e.data)
     }
@@ -11,6 +11,6 @@ async function saveDocument() {
 
 function loadDocument(data) {
   console.log("load")
-  TXTextControl.loadDocument(TXTextControl.StreamType.HTMLFormat,
+  TXTextControl.loadDocument(TXTextControl.StreamType.RichTextFormat,
     btoa(data));
 }
